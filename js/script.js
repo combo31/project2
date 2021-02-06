11460af0cbac01896ecb03cccb85fc08699a58ef
combo31 <combo.1990@mail.ru>
2021-02-06 06:41:57 +0300

    commit2

--- js/script.js
+++ js/script.js
@@ -1,58 +1,108 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-//Вариант 1
-adv.forEach(item => {
-    item.remove();
-});
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
 
-// Вариант 2
-// adv.forEach(function (item) {
-//     item.remove();
-// });
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+        //const newFilmCircumcised = newFilm.slice(22) + '...';
+        if (newFilm) { // if (newFilm.length < 22)
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if (favorite) {
+                console.log('Добавляем любимый фильм');
+            }
+            movieDB.movies.push(newFilm);
+
+            sortArr(movieDB.movies); 
+
+            createMovieList(movieDB.movies, movieList);
+
+         } 
+         //else {
+
+        //     movieDB.movies.push(newFilmCircumcised) ;
+
+        //     sortArr(movieDB.movies); 
+
+        //     createMovieList(movieDB.movies, movieList);
+        // }
+
+        event.target.reset();
+
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
 
 
+    const makeChanges = () => {
+        genre.textContent = 'Драма'; // только текст
 
-genre.textContent = 'Драма'; // только текст
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
 
-movieList.innerHTML = "";
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+ 
 
-movieDB.movies.sort();
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        // добавляем сортировку
+        sortArr(films);
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                 </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) =>{
+            // где btn - корзина, i - номер фильма по порядку в массиве
+            btn.addEventListener('click', () => {// навешиваем клик
+                btn.parentElement.remove();// удаляем родителя
+                movieDB.movies.splice(i, 1); // удаляем из БД
+                // где i-номер c кот. удаляем, 1- сколько элементов удалить
+                //применим рекурсию для изменения нумерации
+                createMovieList(films, parent);
+
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 });
